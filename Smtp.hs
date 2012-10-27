161e6c37cd6ad1e8f26bb0a9cecefdaec0e769c0
Jingcheng Liu <liuexp@gmail.com>
2012-10-27 23:25:58 +0800

    code cleanup

--- Smtp.hs
+++ Smtp.hs
@@ -12,7 +12,6 @@ import Text.ParserCombinators.Parsec.Rfc2822 (
         Field(..),
         NameAddr(..)
     )
-import Data.IORef (newIORef, readIORef)
 import Network.Socket
 import Control.Exception	(bracketOnError,finally)
 import System.IO
@@ -20,17 +19,17 @@ import Control.Monad		(forM_)
 import Data.List		(intercalate)
 import System.Time
 import System.Locale
+import Data.Maybe
 
 simpleMakeMessage title content from to ct=
 		Message [From [NameAddr (Just from) from], To [NameAddr (Just to) to], Subject title, Date ct] content
 
 combineMessages = intercalate "\r\n"
 
-formatName xx@(NameAddr mName addr)=(case mName of
-						Just y -> show y
-						Nothing -> ""
-						) ++ angleAddr addr ""
+formatName xx@(NameAddr mName addr)=fromMaybe "" mName ++ angleAddr addr ""
+
 formatNameList x =intercalate ",\n\t" $ map formatName x
+
 formatFields (Sender x) = "From: " ++ formatName x
 formatFields (From x) = "From: " ++ formatNameList x
 formatFields (Subject x) = "Subject: " ++ x
@@ -41,26 +40,19 @@ formatFields (Date x) = "Date: " ++ formatCalendarTime defaultTimeLocale "%a, %d
 formatFields _ = ""
 
 formatMessage :: Message -> String
-formatMessage message@(Message fields body) = combineMessages $ escapeDot (fFields ++ [""] ++ fBody) where
+formatMessage (Message fields body) = combineMessages $ escapeDot (fFields ++ [""] ++ fBody) where
 							fBody = map (reverse . dropWhile (=='\r') . reverse ) $ lines body
 	    						fFields = concatMap (lines. formatFields) fields
 							escapeDot = map (\x -> case x of
 										"." -> ". "
-										otherwise-> x) 
+										_ -> x) 
 
 
 
 angleAddr :: String -> ShowS
 angleAddr addr = (("<"++addr++">") ++)
 
-sendMail :: (String -> IO()) -> String -> String -> String -> Message -> IO()
-sendMail log heloDomain smtpHost smtpPort message =
-		getAddrInfo Nothing (Just smtpHost) (Just smtpPort) >>=
-		(\smtpAddr -> sendSMTP log heloDomain (head smtpAddr) message) 
-		-- TODO : error handling
---		(\status -> case head status of
---					       Nothing -> return ()
---					       Just x -> log $ "SMTP send failed" ++ show x)
+sendMail = sendSMTP
 
 sendSMTP :: (String -> IO()) -> String -> AddrInfo -> Message -> IO()
 sendSMTP log heloDomain smtpAddr message = 
@@ -94,6 +86,7 @@ sendSMTP log heloDomain smtpAddr message =
 				, formatMessage message
 				, "."
 				, "QUIT"]
+				-- TODO : error handling
 				$ \ line -> hPutStr h line >> hPutStr h "\r\n" >>log line>>log "\r\n"
 			hFlush h
   			)`finally` hClose h
